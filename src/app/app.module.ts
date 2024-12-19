62716b3f9ef3382c0d6675d5b6cbe0640d025629
gabriel <gabrielsillva356@gmail.com>
2024-12-18 21:11:16 -0300

    versão inicial de sistema de postagens de momentos com mensageria e edição de posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { AboutComponent } from './componentes/pages/about/about.component';
+import { HomeComponent } from './componentes/pages/home/home.component';
+import { NewMomentComponent } from './componentes/pages/new-moment/new-moment.component';
+import { MomentFormComponent } from './componentes/moment-form/moment-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    NewMomentComponent,
+    MomentFormComponent
   ],
   imports: [
     BrowserModule,
