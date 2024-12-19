700381b527397e6a2364ee08c78f327ab283ee02
gabriel <gabrielsillva356@gmail.com>
2024-12-18 22:45:34 -0300

    criaçao de validações com rective forms, criação de interfaces e serviços para comunicação com o backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import{HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 import { HeaderComponent } from './componentes/header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { AboutComponent } from './componentes/pages/about/about.component';
 import { HomeComponent } from './componentes/pages/home/home.component';
 import { NewMomentComponent } from './componentes/pages/new-moment/new-moment.component';
 import { MomentFormComponent } from './componentes/moment-form/moment-form.component';
+
 
 
 @NgModule({
@@ -23,7 +29,10 @@ import { MomentFormComponent } from './componentes/moment-form/moment-form.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
